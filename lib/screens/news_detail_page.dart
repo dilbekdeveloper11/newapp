313c4d8dd0f66e4c28c249a810c117ff9a54c841
dilbekDeveloper <dilbekdeveloper23@gmail.com>
2022-05-16 19:49:32 +0500

    news Detail pageda stack ishlatildi

--- lib/screens/news_detail_page.dart
+++ lib/screens/news_detail_page.dart
@@ -18,7 +18,7 @@ class NewsDetailPage extends StatelessWidget {
         child: Stack(
           children: [
             Positioned(
-              top: getHeight(374),
+              top: getHeight(404),
               child: Container(
                 height: getHeight(438),
                 width: getWidht(375),
@@ -34,7 +34,12 @@ class NewsDetailPage extends StatelessWidget {
                   ),
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.all(18.0),
+                  padding: EdgeInsets.only(
+                    top: getHeight(40),
+                    left: getWidht(10),
+                    right: getWidht(10),
+                    bottom: getHeight(10),
+                  ),
                   child: Text(
                     "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
                     style: TextStyle(
@@ -44,7 +49,44 @@ class NewsDetailPage extends StatelessWidget {
                   ),
                 ),
               ),
-            )
+            ),
+            Positioned(
+              height: getHeight(1464),
+              left: getWidht(284),
+              child: Container(
+                height: getHeight(56),
+                width: getWidht(56),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "assets/images/FAB.png",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: getHeight(294),
+              left: getWidht(32),
+              child: Container(
+                height: getHeight(141),
+                width: getWidht(311),
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(getHeight(16)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    "",
+                    style: TextStyle(
+                      fontSize: getHeight(14),
+                      color: const Color(0xff2E0505),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
